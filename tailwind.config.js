180e16bd71f4c746dfbf0d3af3c5b4a5c05337bd
Shashank Shekhar Barik <ssbarik.sf@gmail.com>
2025-04-08 21:59:53 +0530

    chore: integrate react-dntel-form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-import path from "path";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    path.resolve(__dirname, "../dev/react-dntel-form/src/**/*.{js,jsx,ts,tsx}"),
+    "./node_modules/react-dntel-form/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
